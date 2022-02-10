cb0dda3951ce8c6ce53cc4905fd41c4addc44d5c
Mert1s <andgtc@gmail.com>
2022-02-10 09:31:46 -0300

    Simplificado o update do projeto

--- src/services/projeto.service.ts
+++ src/services/projeto.service.ts
@@ -35,39 +35,18 @@ export class ProjetoService {
 
     async update(id: number, body: Projeto) {
         if (await this.find(id)) {
-            if (body.fim !== undefined && body.inicio > body.fim) // Se o usuario fornecedor a data de fim, ela não pode ser inferior a data de inicio
-                throw new HttpException('A data de fim não pode ser inferior a data de inicio', 500);
-
-            const colaboradores = body.colaboradores == undefined ? [] : body.colaboradores.map(valor => valor.id); // Lista de colaboradores que recebemos do front-end para atualizar no projeto
-            const result =
-                await createQueryBuilder('projeto_colaboradores_colaborador', 'pcc') // Filtrando a tabela ManyToMany
-                    .innerJoinAndSelect('colaborador', 'c')
-                    .innerJoinAndSelect('projeto', 'p')
-                    .where('c.id = pcc_colaboradorId')// ON do inner join
-                    .andWhere('p_id = pcc_projetoId') // ON do inner join
-                    .andWhere('p_ativo = true')
-                    .andWhere('c_ativo = true')
-                    .andWhere("date(:data) between date(p.inicio) and ifnull(fim,date('now'))", { data: body.inicio }) // Filtra se a data do projeto corrente é está entre a data de algum projeto em andamento
-                    .andWhere('pcc_colaboradorId in (:...colaboradores)', { colaboradores: colaboradores }) // Lista de colaboradores que irá chegar do front-end
-                    .andWhere('pcc_projetoId <> :id', { id: id }) // Não filtrar pelo mesmo projeto
-                    .getRawMany();
-            if (!!result.length) { // Se for encontrado mais de 1 registro não é para atualizar 
-                const ColaboradoresJaPresentesEmOutrosProjetos = result.map(value => value.c_nome) // Pegar o nome de todos os colaboradores que já estão presentes em um projeto para devolver para o front-end
-                throw new HttpException(`Não pode fazer associar colaboradores que já estão presentes em outros projetos ativos. São estes: ${ColaboradoresJaPresentesEmOutrosProjetos}`, 500)
+            //await this.model.update({ id: id }, body)       // Estava dando erro ao fazer o update, foi trocado pelo create && save que funciona perfeitamente
+            try {
+                body.id = id; // Para não dar erro de UNIQUE na hora de gravar no banco
+                this.validarData(body) // Verifca se a data de inicio é menor que a data de inicio
+                await this.validarColaborador(body, id) // Verifica se os colaboradores enviados estão presentes em outros projetos no mesmo período
+                const projetoAtualizado = await this.model.create(body) // Instancia o objeto para ativar os gatilhos no ENTITY
+                await this.model.save(projetoAtualizado)                   // Aqui eu sobreponho o registro com as alterações enviadas pelo front-end
+                return "Sucesso ao alterar a tarefa do id: " + id;
             }
-            else {                
-                //await this.model.update({ id: id }, body)       // Estava dando erro ao fazer o update, foi trocado pelo save que funciona perfeitamente
-                try {
-                    body.id = id;
-                    const projetoAtualizado = await this.model.create(body)
-                    await this.model.save(projetoAtualizado)                   // Aqui eu sobreponho o registro com as alterações enviadas pelo front-end
-                    return "Sucesso ao alterar a tarefa do id: " + id;
-                }
-                catch (e) {
-                    console.log(e)
-                    console.log(result)
-                    throw new HttpException('Ocorreu algum erro durante a atualização do dados por favor verificar o log', 500);
-                }
+            catch (e) {
+                console.log(e)
+                throw new HttpException('Ocorreu algum erro durante a atualização do dados por favor verificar o log', 500);
             }
         }
         else {
@@ -83,5 +62,31 @@ export class ProjetoService {
         else {
             throw new HttpException(`Não foi possível excluir o projeto do ID: ${id}`, 500)
         }
+    }
+
+    validarData(body: Projeto) {
+        if (body.fim !== undefined && body.inicio > body.fim) // Se o usuario fornecedor a data de fim, ela não pode ser inferior a data de inicio
+            throw new HttpException('A data de fim não pode ser inferior a data de inicio', 500);
+    }
+    async validarColaborador(body: Projeto, id: number): Promise<Projeto[]> {
+        const colaboradores = body.colaboradores == undefined ? [] : body.colaboradores.map(valor => valor.id); // Lista de colaboradores que recebemos do front-end para atualizar no projeto
+        const result =
+            await createQueryBuilder('projeto_colaboradores_colaborador', 'pcc') // Filtrando a tabela ManyToMany
+                .innerJoinAndSelect('colaborador', 'c')
+                .innerJoinAndSelect('projeto', 'p')
+                .where('c.id = pcc_colaboradorId')// ON do inner join
+                .andWhere('p_id = pcc_projetoId') // ON do inner join
+                .andWhere('p_ativo = true')
+                .andWhere('c_ativo = true')
+                .andWhere("date(:data) between date(p.inicio) and ifnull(fim,date('now'))", { data: body.inicio }) // Filtra se a data do projeto corrente é está entre a data de algum projeto em andamento
+                .andWhere('pcc_colaboradorId in (:...colaboradores)', { colaboradores: colaboradores }) // Lista de colaboradores que irá chegar do front-end
+                .andWhere('pcc_projetoId <> :id', { id: id }) // Não filtrar pelo mesmo projeto
+                .getRawMany();
+
+        if (!!result.length) { // Se for encontrado mais de 1 registro não é para atualizar 
+            const ColaboradoresJaPresentesEmOutrosProjetos = result.map(value => value['c_nome']) // Pegar o nome de todos os colaboradores que já estão presentes em um projeto para devolver para o front-end
+            throw new HttpException(`Não pode fazer associar colaboradores que já estão presentes em outros projetos ativos. São estes: ${ColaboradoresJaPresentesEmOutrosProjetos}`, 500)
+        }
+        return result
     }
 }
